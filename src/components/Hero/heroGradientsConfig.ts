34ca58366d909ac99749d8d605c532b15d46753f
raqib <raqib.rasheed@techverito.com>
2024-06-14 14:31:25 +0530

    refactor: Hero files

---
+++ src/components/Hero/heroGradientsConfig.ts
@@ -0,0 +1,17 @@
+export const gradientCirclesConfig = [
+  {
+    className: "gradient-circle green",
+    src: "/hero-circle-green.svg",
+    alt: "hero section gradient circle",
+  },
+  {
+    className: "gradient-circle green",
+    src: "/hero-circle-purple.svg",
+    alt: "hero section gradient circle",
+  },
+  {
+    className: "gradient-circle grenn right",
+    src: "/hero-circle-green.svg",
+    alt: "hero section gradient circle",
+  },
+];
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,60 +1,38 @@
 import { Button, Input } from "antd";
 import "./index.scss";
+import { gradientCirclesConfig } from "./heroGradientsConfig";
+import { ChatScreenDemoImage } from "./ChatScreenImage";
 
 export default function Hero() {
-  return (
-    <section className="hero">
+  const Title = (
+    <>
       <h1>Stay close to your</h1>
       <h1>favorite people.</h1>
-      <div className="phone-number-input">
-        <Input
-          suffix={<Button type="primary">Get started</Button>}
-          size="large"
-          placeholder="Enter your phone number"
-        />
-      </div>
-      <img
-        className="gradient-circle green"
-        src="/hero-circle-green.svg"
-        alt="hero section gradient circle"
+    </>
+  );
+
+  const PhoneNumberInput = (
+    <div className="phone-number-input">
+      <Input
+        suffix={<Button type="primary">Get started</Button>}
+        size="large"
+        placeholder="Enter your phone number"
       />
-      <img
-        className="gradient-circle purple"
-        src="/hero-circle-purple.svg"
-        alt="hero section gradient circle"
-      />
-      <img
-        className="gradient-circle green right"
-        src="/hero-circle-green.svg"
-        alt="hero section gradient circle"
-      />
-      <div className="mobile-screen-images">
+    </div>
+  );
+
+  return (
+    <section className="hero">
+      {Title}
+      {PhoneNumberInput}
+      {gradientCirclesConfig.map((gradient) => (
         <img
-          alt="mobile screen image"
-          className="mobile-screen"
-          src="/hero-mobile-screen.svg"
+          alt={gradient.alt}
+          src={gradient.src}
+          className={gradient.className}
         />
-        <img
-          alt="mobile screen image"
-          className="voice-note"
-          src="/hero-voice-note.svg"
-        />
-        <div className="gift-notification">
-          <img
-            width="50rem"
-            height="50rem"
-            src="/hero-giftbox.svg"
-            alt="gift box image"
-          />
-          <h2>You received a Gift</h2>
-          <Button>view gift</Button>
-        </div>
-        <img
-          alt="girl face image"
-          className="girl-face"
-          src="/hero-girl-face.svg"
-        />
-      </div>
+      ))}
+      <ChatScreenDemoImage />
     </section>
   );
 }
